d41df1b16998d40115bf5e1312f18f9faf1d7a6f
Zain ul abdin <zainburfat112@gmail.com>
2021-03-19 13:28:38 +0500

    ok

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ include 'include/head.php';
           <div class="w3-panel w3-blue w3-center">
             <h1 class="w3-text-orange" style="text-shadow:1px 1px 0 #444">
               <img style="width: 100px;" src="logo.jpg" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-              <b>Welcome to the Farmer Connection</b>
+              <b>Welcome to the Farmer </b>
             </h1>
           </div>
         </div>
